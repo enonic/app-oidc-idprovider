532979ea74c2cb0618c963a06854067f0b82ca3d
Espen Norderud <espen42@gmail.com>
2022-11-11 16:06:40 +0100

    Add (PoC-level) parsing of data structures other than plain string values (#946)

--- src/main/resources/lib/config.js
+++ src/main/resources/lib/config.js
@@ -7,6 +7,7 @@ const portalLib = require('/lib/xp/portal');
  *  That is, the config keys are dot-separated with exactly two subfields, and the first subfield is the id provider key, eg. 'oidc.authorizationUrl'.
  *  Skips keys that don't match this pattern, and log-warns about mismatched keys starting with the id provider key.
  *  Returns an object where the keys are the second subfield from well-formed configs, eg. { authorizationUrl: 'http://something', clientSecret: 'vs12jn56bn2ai4sjf' }, etc
+ *  If no valid keys are found, returns null.
  */
 function getIdProviderConfigFromFile() {
     const idProviderKey = portalLib.getIdProviderKey();
@@ -40,7 +41,9 @@ function getIdProviderConfigFromFile() {
         .filter( key => key);
 
     if (skipped.length) {
-        log.warning(`${app.name}.cfg contains invalid config key(s) for the '${idProviderKey}' ID provider. They should only be on this shape: '${idProviderKey}.oneSingleSubkey = value'. Skipping config(s): ${JSON.stringify(skipped)}`);
+        log.warning(`${app.name}.cfg contains invalid config key(s) for the '${idProviderKey}' ID provider - the entries should ONLY be on this shape: '${idProviderKey}.oneSingleSubkey = value'.`);
+        log.warning(`    Skipping config(s): ${JSON.stringify(skipped)}`);
+        log.warning(`    (If deeper tree structures (eg. more than one single subkey) are needed, or other data structures such as array values, enter a value on the shape of a JSON.parse-able string)`)
     }
 
     if (!idProviderConfigKeys || !idProviderConfigKeys.length) {
@@ -49,7 +52,12 @@ function getIdProviderConfigFromFile() {
 
     const idProviderConfig = {};
     idProviderConfigKeys.forEach(key => {
-        idProviderConfig[key] = app.config[`${idProviderKey}.${key}`];
+        const value = app.config[`${idProviderKey}.${key}`];
+        try {
+            idProviderConfig[key] = JSON.parse(value);
+        } catch (e) {
+            idProviderConfig[key] = value;
+        }
     });
 
     log.debug(`Found config for the '${idProviderKey}' ID provider in ${app.name}.cfg. Using that instead of node-stored config from authLib.`);
@@ -58,7 +66,7 @@ function getIdProviderConfigFromFile() {
 
 function getIdProviderConfig() {
 
-    let idProviderConfig = getIdProviderConfigFromFile() || authLib.getIdProviderConfig();
+    const idProviderConfig = getIdProviderConfigFromFile() || authLib.getIdProviderConfig();
     preconditions.checkConfig(idProviderConfig, 'issuer');
     preconditions.checkConfig(idProviderConfig, 'authorizationUrl');
     preconditions.checkConfig(idProviderConfig, 'tokenUrl');
