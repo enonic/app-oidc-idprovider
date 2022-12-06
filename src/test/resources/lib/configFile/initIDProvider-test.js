fe9a0fcb2d240a46368827c926883d4ff54ab1a2
Espen Norderud <espen42@gmail.com>
2022-12-06 22:58:04 +0100

    Add unit test for initIDProvider, still doesn't initialize (#116)

--- src/test/resources/lib/configFile/initIDProvider-test.js
+++ src/test/resources/lib/configFile/initIDProvider-test.js
@@ -1,14 +1,15 @@
 const lib = require('./initIDProvider');
 const test = require('/lib/xp/testing');
 
-exports.test_initIDProvider_recognizesGenericKey_Defaultgroups = () => {
-    const CREATED_IDPS = [];
+exports.test_initIDProvider_initUserStores_addOnlyNewIDPs_ifShouldAutoInit = () => {
+    const CREATED_IDPS = {};
+    let addedProviders = 0;
 
     test.mock("/lib/configFile/configFile.js", {
         getAllIdProviderNames: () => ["myidp1", "myidp2", "myidp3", , "myidp4"],
         shouldAutoInit: () => true,
-        getConfigForIdProvider: () => ({
-            displayName: "MockName",
+        getConfigForIdProvider: (idProviderName) => ({
+            displayName: "Mock " + idProviderName,
             description: "Mock description",
         })
     });
@@ -16,7 +17,8 @@ exports.test_initIDProvider_recognizesGenericKey_Defaultgroups = () => {
     test.mock("/lib/configFile/services/bean.js", {
         getIdProviders: () => ["myidp1", "myidp2"],
         createIdProvider: (params) => {
-            CREATED_IDPS.push(params);
+            CREATED_IDPS[params.name] = params;
+            addedProviders++;
             return true;
         }
     });
@@ -28,7 +30,65 @@ exports.test_initIDProvider_recognizesGenericKey_Defaultgroups = () => {
 																																(typeof CREATED_IDPS + (CREATED_IDPS && typeof CREATED_IDPS === 'object' ? (" with keys: " + JSON.stringify(Object.keys(CREATED_IDPS))) : ""))
 																															) + "): " + JSON.stringify(CREATED_IDPS, null, 2)
 																														);
+    // Only myidp3 and -4 should have been added, since myidp1 and -2 are already in system repo,
+    // according to the mocked getIdProviders:
 
+    test.assertEquals(addedProviders, 2);
+
+    test.assertEquals(test['myidp1'], undefined);
+    test.assertEquals(test['myidp2'], undefined);
+
+    test.assertEquals(test['myidp3'].name, "Mock myidp3");
+    test.assertEquals(test['myidp3'].idProviderConfig.applicationKey, app.name);
+    test.assertEquals(test['myidp3'].permissions, []);
+
+    test.assertEquals(test['myidp4'].name, "Mock myidp4");
+    test.assertEquals(test['myidp4'].idProviderConfig.applicationKey, app.name);
+    test.assertEquals(test['myidp4'].permissions, []);
+};
+
+exports.test_initIDProvider_initUserStores_addNoNewIDPs_ifNoAutoInit = () => {
+    const CREATED_IDPS = {};
+    let addedProviders = 0;
+
+    test.mock("/lib/configFile/configFile.js", {
+
+        // The important difference:
+        shouldAutoInit: () => false,
+
+        getAllIdProviderNames: () => ["myidp1", "myidp2", "myidp3", , "myidp4"],
+        getConfigForIdProvider: (idProviderName) => ({
+            displayName: "Mock " + idProviderName,
+            description: "Mock description",
+        })
+    });
+
+    test.mock("/lib/configFile/services/bean.js", {
+        getIdProviders: () => ["myidp1", "myidp2"],
+        createIdProvider: (params) => {
+            CREATED_IDPS[params.name] = params;
+            addedProviders++;
+            return true;
+        }
+    });
+
+    lib.initUserStores();
+    log.info("CREATED_IDPS (" +
+        (Array.isArray(CREATED_IDPS) ?
+                ("array[" + CREATED_IDPS.length + "]") :
+                (typeof CREATED_IDPS + (CREATED_IDPS && typeof CREATED_IDPS === 'object' ? (" with keys: " + JSON.stringify(Object.keys(CREATED_IDPS))) : ""))
+        ) + "): " + JSON.stringify(CREATED_IDPS, null, 2)
+    );
+
+    //  myidp1 and -2 are already in system repo,
+    // but shouldAutoInit = false (mocked):
+
+    test.assertEquals(addedProviders, 0);
+
+    test.assertEquals(test['myidp1'], undefined);
+    test.assertEquals(test['myidp2'], undefined);
+    test.assertEquals(test['myidp3'], undefined);
+    test.assertEquals(test['myidp4'], undefined);
 };
 
 
