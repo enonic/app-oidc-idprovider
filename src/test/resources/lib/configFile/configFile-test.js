367e9f4e3954a293931dc33d57f28e23f0b1a6e6
Espen Norderud <espen42@gmail.com>
2022-12-06 13:36:37 +0100

    Add unit tests for configFile/parsingCallbacks (#116)

--- src/test/resources/lib/configFile/configFile-test.js
+++ src/test/resources/lib/configFile/configFile-test.js
@@ -1,3 +1,15 @@
-var pcb = require('./parsingCallbacks-test');
+const parsingCallbacksTests = require('./parsingCallbacks-test');
+const initIDProviderTests = require('./initIDProvider-test');
 
-exports.testParsingCallbacks = pcb.testParsingCallbacks;
+const addTests = (testLib, source) => {
+    Object.keys(testLib).forEach( key => {
+        // log.info(key + ": " + typeof testLib[key]);
+        if (typeof testLib[key] === 'function' && key.startsWith('test_')) {
+            exports[key] = testLib[key];
+            log.info(`Added to tests: ${key} from ${source}`);
+        }
+    })
+}
+
+addTests(parsingCallbacksTests, "parsingCallbacks-tests.js");
+addTests(initIDProviderTests, "initIDProvider-test.js");
