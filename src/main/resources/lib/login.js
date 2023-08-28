3b7fb5cdfb1203f18cdff55be173cf35e930eb3c
Bradnitski <vbr@enonic.com>
2023-08-28 11:17:26 +0200

    login fails for non-existent user #147

--- src/main/resources/lib/login.js
+++ src/main/resources/lib/login.js
@@ -23,7 +23,7 @@ function login(claims) {
         //Creates the users
         const idProviderConfig = configLib.getIdProviderConfig();
 
-        if (String(idProviderConfig.rules?.forceEmailVerification) === 'true') {
+        if (idProviderConfig.rules && String(idProviderConfig.rules.forceEmailVerification) === 'true') {
             preconditions.check(userinfoClaims.email_verified === true, 'Email must be verified');
         }
 
